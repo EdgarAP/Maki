fa0143a8d64b965d48bc3630dc8731217626a011
c.salgado <c.salgado@sesderma.com>
2016-10-25 18:59:34 +0200

    A ver si ya...

---
+++ Maki Installer/Business/MakiInstaller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using static Maki_Installer.Business.Package;
+
+namespace Maki_Installer.Business
+{
+    class MakiInstaller
+    {
+        internal RepositoryPackage repo = new RepositoryPackage();
+        internal InstalledPackage insta = new InstalledPackage();
+        internal PinnedPackage pin = new PinnedPackage();
+        internal PSmanager man = new PSmanager();
+
+        internal void updateRemoteRepository()
+        {
+            Collection<PSObject> bruto = man.getPackageListRemote();
+            repo.repository = convertPSOtoOGP(bruto);
+        }
+        private void updateLocalRepository()
+        {
+            Collection<PSObject> bruto = man.getPackageListLocal();
+            repo.repository = convertPSOtoOGP(bruto);
+        }
+        internal Collection<string> installFirefox()
+        {
+            return man.install("firefox");
+        }
+        internal Collection<OneGetPackage> getAllPackages()
+        {
+            updateLocalRepository();
+            return repo.repository;
+        }
+
+    }
+}
