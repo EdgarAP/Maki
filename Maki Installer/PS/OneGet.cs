e2694af844b535ba8440482f9fd77b75f85e2648
c.salgado <c.salgado@sesderma.com>
2016-10-27 11:52:39 +0200

    Añadido script de instalación de chocolatey

---
+++ Maki Installer/PS/OneGet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+
+namespace Maki_Installer.PS
+{
+    class OneGet : PSInvoker
+    { //Carlos Salgado
+
+        internal Collection<PSObject> getPackageListRemote()
+        { //no va, find-package no devuelve lo que toca. 
+            return invokePSObject("$obj=find-package -source chocolatey|select -Property 'name','version','summary','status';$obj|Export-Clixml "+ StaticInfo.repositoryPkgPath + "\\Repository.xml -Force;");
+        }
+        internal Collection<PSObject> getPackageListLocal()
+        {
+            return invokePSObject("Import-Clixml " + StaticInfo.repositoryPkgPath);
+        }
+    }
+}
